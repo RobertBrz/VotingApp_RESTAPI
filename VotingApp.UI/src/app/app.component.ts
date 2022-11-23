a88ae8a2583deb9c52d786843fb1be94a399b5c5
robert.brzozowski <noemail>
2022-11-23 11:24:44 +0100

    fixed issue

--- VotingApp.UI/src/app/app.component.ts
+++ VotingApp.UI/src/app/app.component.ts
@@ -1,33 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { VoterService } from 'src/Services/voter.service'
-import { IVoter } from '../Shared/interfaces';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public forecasts?: WeatherForecast[];
-  public voters?: Observable<IVoter[]>;
-
-  constructor(http: HttpClient,
-    private voterService: VoterService) {
-    //http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-    //  this.forecasts = result;
-    //}, error => console.error(error));
-    this.getVoters();
-  }
 
   title = 'VotingApp.UI';
 
-  getVoters() {
-    var voters = this.voterService.getVoters();
-    //voters.forEach(v => console.log(v))
-    voters.forEach(item => console.log(item));
+  constructor(http: HttpClient) {
+    //http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+    //  this.forecasts = result;
+    //}, error => console0.error(error));
   }
 }
 
--- VotingApp.UI/src/app/app.module.ts
+++ VotingApp.UI/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { VoterComponent } from 'src/voter/voter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, VoterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
